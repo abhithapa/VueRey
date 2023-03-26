bb37ae523dac7a0fe1abc5deaad8e23b272d4c5d
abhithapa <71200724+abhithapa@users.noreply.github.com>
2023-03-27 00:02:36 +0545

    container

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screens/home_page.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  load() {
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +24,24 @@ class SplashScreen extends StatelessWidget {
       body: Container(
         height: size.height,
         width: size.width,
-        
         child: Column(
-          children: [Image.asset('asstes/images/splash')],
-          
+          children: [
+            Image.asset('asstes/images/splash.png'),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 50,
+              width: size.width * 0.7,
+              decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Text(
+                'Skip',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            )
+          ],
         ),
       ),
     );
